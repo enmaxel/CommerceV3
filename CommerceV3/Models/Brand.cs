ab87b02e20a1caa7d304a01af80bda09b5ebd5b5
Kaan Adıgüzel <kaanadiguzel@msn.com>
2018-07-20 17:59:03 +0300

    models created to be con.

---
+++ CommerceV3/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceV3.Models
+{
+    public class Brand
+    {
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Logo { get; set; }
+
+		public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
